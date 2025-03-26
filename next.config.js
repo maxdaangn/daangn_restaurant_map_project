d5298c31b2f9aead83e8c8774a451cae98fefeb8
Max <max@Karrot-Max-G5XVYHYTGG.local>
2025-03-27 01:46:15 +0900

    GitHub Pages 배포를 위한 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/daangn_restaurant_map_project' : '',
   images: {
-    domains: ['maps.googleapis.com'],
+    unoptimized: true,
   },
 }
 
